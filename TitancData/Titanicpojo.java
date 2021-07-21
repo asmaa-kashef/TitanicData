9ed36a3c9642de7e4fc8de24c7d35db7fb28b514
asmaa-kashef <58347664+asmaa-kashef@users.noreply.github.com>
2021-07-21 04:10:12 +0200

    Add files via upload

---
+++ TitancData/Titanicpojo.java
@@ -0,0 +1,124 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author asmaa
+ */
+public class Titanicpojo {
+    
+    private String pclass;
+    private String survived;
+    private String name;
+    private String sex;
+    private float age;
+    private String  sibsp;
+    private String  parch;
+    private String  ticket;
+    private Float fare;
+    private String cabin;
+    private String embarked;
+    private String  boat;
+    private String home_dest;
+    
+    public String  getpclass(){
+        return  pclass;
+    }
+    public void setpclass(String  Pclass){
+        this.pclass=Pclass;
+    }
+    public String  getsurvived(){
+        return survived;
+    }
+    public void setsurvived(String  Survived){
+        this.survived=Survived;
+    }
+    public String getname(){
+        return name;
+    }
+    public void setname(String Name){
+        this.name=Name;
+    }
+
+    public String getsex() {
+        return sex;
+    }
+
+    public void setsex(String Sex) {
+        this.sex = Sex;
+    }
+
+    public float getage() {
+        return age;
+    }
+
+    public void setage(float Age) {
+        this.age = Age;
+    }
+
+    public String getsibsp() {
+        return sibsp;
+    }
+
+    public void setsibsp(String Sibsp) {
+        this.sibsp = Sibsp;
+    }
+
+    public String getparch() {
+        return parch;
+    }
+
+    public void setparch(String Parch) {
+        this.parch = Parch;
+    }
+        public String getticket() {
+        return ticket;
+    }
+
+    public void setticket(String Ticket) {
+        this.ticket= Ticket;
+    }
+        public float getfare() {
+        return fare;
+    }
+
+    public void setfare(float Fare) {
+        this.fare= Fare;
+    }
+    public String getcabin() {
+        return cabin;
+    }
+    public void setcabin(String Cabin) {
+        this.cabin= Cabin;
+    }
+         public String getembarked() {
+        return embarked;
+    }
+
+    public void setembarked(String Embarked) {
+        this. embarked =  Embarked;
+    }
+    public String getboat() {
+        return boat;
+    }
+    public void setboat(String Boat) {
+        this.boat = Boat;
+    }
+       public String gethome_dest() {
+        return home_dest;
+    }
+    public void sethome_dest(String Home_dest) {
+        this.home_dest = Home_dest;
+    }
+           
+
+   
+}
+    
+
+    
+    
+    
